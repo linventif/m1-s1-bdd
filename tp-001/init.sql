a856ec7e000f35d4f2a79bd17e2aaa2075f33f02
linventif <gregoire.becue@pm.me>
2025-09-16 12:07:45 +0200

    feat: add questions for tp1

--- tp-001/init.sql
+++ tp-001/init.sql
@@ -54,11 +54,11 @@ NiveauQ Niveau d'une qualification CC(9) énuméré BEP, CAP, Bac Pro, BTS ou In
 NomE Nom d'un employé CC(40)
 NomGA Nom d'une gamme de travaux CC(40)
 énuméré
-Cf. tableau joint
+# Cf. tableau joint
 NomQ Nom d'une qualification CC(40)
 NomTY Nom d'un type de travaux (maçonnerie, carrelage…) CC(40)
 énuméré
-Cf. tableau joint
+# Cf. tableau joint
 NomV Nom d'une ville CC(40)
 PrenomE Prénom d'un employé CC(40)
 RaisonSoc Raison Sociale d'une entreprise CC(40)
@@ -188,3 +188,54 @@ CREATE TABLE TRAVAILLERDEVIS (
     FOREIGN KEY (CodeE) REFERENCES EMPLOYE(CodeE),
     FOREIGN KEY (CodeTY) REFERENCES TYPETRAVAUX(CodeTY)
 );
+
+# Questions
+# 1. Code et adresse postale complète (rue et ville) des chantiers ayant fait intervenir des employés dans le cadre
+# de devis
+# 2. Nom et Prénom des employés ayant un niveau d’étude supérieur au Bac (BTS ou ingénieur)
+# 3. Codes des départements et noms des villes accueillant des entreprises et des chantiers (classement par ordre
+# « alphabétique » des départements et villes)
+# 4. Nom des types de travaux dont le coût horaire employé représente plus de 80% du cout horaire client.
+# 5. Nom des types de travaux ayant nécessité des réalisations hors devis cette année
+# 6. Raison Sociale des entreprises ayant proposé des devis cette année dans les villes où elles sont implantées
+# 7. Pour chaque gamme de travaux, donner son nom et le nom des types de travaux associés s’il en possède
+# 8. Nom des gammes et des types de travaux rentables (types de travaux dont le gain entre le tarif horaire payé par le client et le tarif horaire payé aux employés est supérieur à 80% du tarif horaire employé) avec en-têtes
+# personnalisés et classement décroissant des rentabilités
+# 9. Nom des types de travaux rentables proposés ce mois-ci dans des devis
+# 10. Code et ville des chantiers ayant été réalisés avec des travaux répondant à des devis et des travaux hors devis
+# 11. Code et ville des chantiers n’ayant pas fait l’objet d’un devis
+# 12. Code et ville des chantiers n’ayant été réalisés qu’avec des travaux répondant à des devis
+# 13. Code des chantiers n’ayant pas fait l’objet de travaux
+# 14. Nom et prénom des employés ayant une qualification relative aux gammes Gros œuvre et second œuvre.
+# 15. Raison sociale et groupe des sociétés n’ayant proposé que des devis (aucun travail effectué)
+# 16. Code et date des devis ne proposant que du gros œuvre
+# 17. Code et date des devis proposant du gros œuvre et du second œuvre
+# 18. Tableau de bord contenant, le nom, le prénom et le nombre d’heures maximum réalisé dans une journée par un employé en réponse à un devis
+# 19. Tableau de bord contenant la raison sociale des sociétés et le Chiffre d’Affaire (CA) réalisé dans le cadre des devis (calculé en fonction des heures déclarées dans le devis), classement par ordre décroissant des CA © Franck RAVAT Page 5 sur 5
+# 20. Code et ville des chantiers totalisant plus de 1000 heures de travail effectué hors devis (classement par ordre décroissant du nombre d’heures)
+# 21. Tableau de bord permettant d’afficher le code des départements et le nombre de chantiers associés en se limitant aux départements possédant aux minimum 5 chantiers
+# 22. Nom des villes à prospecter : celles qui ne possède pas plus de 2 chantiers
+# 23. Donner le nom et le prénom des employés pouvant effectuer des travaux de toutes les gammes.
+# 24. Donner la raison sociale des entreprises de la Haute Garonne (département 31) proposant tous les types de travaux au travers de ses devis
+# 25. Code, date et CA prévisionnel du dernier devis (celui ayant le code le plus élevé)
+# 26. Raison sociale de l’entreprise ayant réalisé un CA prévisionnel supérieur à celui de Bati31
+# 27. Noms des villes possédant plus de chantiers que Toulouse
+# 28. Code et date du dernier devis émis par une SARL
+# 29. Pour chaque entreprise, donner sa raison sociale et son dernier devis (code et date et son Chiffre d’affaires prévisionnel)
+# 30. Donner le CA (Chiffre d'Affaire) prévisionnel moyen d'un devis
+# 31. Donner le nom de la gamme proposant un CA prévisionnel plus élevé que la gamme SOL
+# 32. Donner la raison sociale des entreprises ayant proposé le plus de devis
+# 33. Donner les coordonnées du chantier (code, rue, ville) ayant fait intervenir le plus d’employés cette année (dans le cadre des devis).
+# 34. Pour chaque chantier donner son code et son CA réalisé hors devis
+# 35. Pour chaque chantier, donner son code et son CA réalisé dans le cadre des devis
+# 36. Pour chaque chantier donner son CA total (en se limitant aux chantiers ayant des CA hors devis et des CA devis)
+# 37. Donner le nom des villes possédant le plus chantiers.
+# 38. Donner les noms des types de travaux ayant plus d’heures prévisionnelles totales que le type plomberie
+# 39. Nom et type de la société ayant effectué le plus d’heures hors devis cette année.
+# 40. Donner le nom et le prénom des employés ayant travaillé dans le cadre des devis sur tous les chantiers de la Haute Garonne (département 31) cette année.
+# 41. Code des chantiers n’ayant été réalisés qu’avec des travaux défini dans un devis (pas de travaux hors devis)
+# 42. Donner le code des devis contenant uniquement des travaux des gammes gros œuvre et second œuvre
+# 43. Donner le code des départements n’accueillant aucune société
+# 44. Donner le code du ou des départements accueillant le plus grand nombre de sociétés
+# 45. Donner le code des départements accueillant toutes les sociétés du groupe
+# 46. Donner le code des départements accueillant plus de société que le département 75.
